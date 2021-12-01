b4df4615133cd9b25f34a61236db067ed84427fd
hypothermic <admin@hypothermic.nl>
2021-12-01 07:51:21 +0100

    Day 1
    Signed-off-by: hypothermic <admin@hypothermic.nl>

---
+++ README.md
@@ -0,0 +1,17 @@
+Advent of Code 2021
+=====
+
+My solutions to this year's competition.
+
+I will upload the solutions a few hours later to let everyone finish with their own solutions.
+No peeking :)
+
+| Day | Time required | Finished at (CET) | Global rank | Solution                 |
+| --- | ------------- | ----------------- | ----------- | ------------------------ |
+| 1   | 10:31         | 06:10 AM          | 2608        | [day1.erl](src/day1.erl) |
+
+Build & run
+-----
+
+    $ rebar3 escriptize
+    $ set -m && clear && _build/default/bin/aoc21
---
+++ src/day1.erl
@@ -0,0 +1,35 @@
+-module(day1).
+-author("hypothermic").
+
+-define(INFINITE, math:pow(10,10)).
+
+%% API
+-export([
+	first/1,
+	second/1
+]).
+
+iterate(Current, {Prev, Count}) when Current < Prev ->
+	{Current, Count};
+
+iterate(Current, {_Prev, Count}) ->
+	{Current, Count + 1}.
+
+first(Input) ->
+	{_, Count} = lists:foldl(fun (Current, AccIn) ->
+		iterate(Current, AccIn)
+	end, {?INFINITE, 0}, Input),
+
+	Count.
+
+slide(Amount, [Current, Next, Then | Rest], Prev, Count) when Amount > 0, (Current + Next + Then) > Prev ->
+	slide(Amount-1, [Next, Then] ++ Rest, (Current + Next + Then), Count+1);
+
+slide(Amount, [Current, Next, Then | Rest], _Prev, Count) when Amount > 0 ->
+	slide(Amount-1, [Next, Then] ++ Rest, Current, Count);
+
+slide(_, _, _, Count) ->
+	Count.
+
+second(Input) ->
+	slide(length(Input)-2, Input, ?INFINITE, 0).
